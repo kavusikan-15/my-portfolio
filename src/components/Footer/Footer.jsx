6e2b6133a2d1b12ff5ee016f8c9f98fd037f971d
Kavusikan JK <kavusikanjk15@gmail.com>
2025-08-05 15:17:11 +0530

    Add files via upload

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,21 @@
+import '../../styles/Footer.css';
+
+const Footer = () => {
+  return (
+    <footer>
+      <nav>
+        <div className="nav-links-container">
+          <ul className="nav-links">
+            <li><a href="#about">About</a></li>
+            <li><a href="#experience">Skills</a></li>
+            <li><a href="#projects">Projects</a></li>
+            <li><a href="#contact">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+      <p>Copyright &#169; {new Date().getFullYear()} KAVUSIKAN JK. All Rights Reserved.</p>
+    </footer>
+  );
+};
+
+export default Footer;
